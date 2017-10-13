66de2dd603c4a71080095767f0e6111c8a518914
jlipworth <jonathan.lipworth@gmail.com>
2017-10-13 17:53:38 -0400

    Moved shove into socket to main function

--- src/open-ice-script/open-ice-socket.py
+++ src/open-ice-script/open-ice-socket.py
@@ -4,7 +4,7 @@ metric_ids = {}
 
 
 def broadcast_input():
-    sock = initialize_socket()
+    connect = initialize_socket()
     while True:
         input_word = input()
         if input_word == "metric_id:" and dict.get(input_word, None) is not None:
@@ -15,14 +15,9 @@ def broadcast_input():
                 # if we need more could insert this into dict
                 # seems that every piece of info has this field
                 if input_word == "value:":
-                    shove_into_socket(id_str, input())
+                    connect.send(id_str + " " + input())
                     break
 
-
-def shove_into_socket(sock, id_str, value):
-    connect, address = sock.accept()
-    sock.send(str(id_str + " " + value))
-   
 
 def initialize_socket():
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -30,7 +25,8 @@ def initialize_socket():
     port = 56754
     sock.bind(host, port)
     sock.listen(5)
-    return sock
+    connect, address = sock.accept()
+    return connect
 
 
 
