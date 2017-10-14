11bd82be3618f99ce3738a401666200f73c3a426
Jingyi Qing <jyqing@umich.edu>
2017-10-13 21:51:19 -0400

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pRAM = new System.Diagnostics.PerformanceCounter();
+            this.pCPU = new System.Diagnostics.PerformanceCounter();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBarCPU = new MetroFramework.Controls.MetroProgressBar();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBarRAM = new MetroFramework.Controls.MetroProgressBar();
+            this.labelCPU = new MetroFramework.Controls.MetroLabel();
+            this.labelRAM = new MetroFramework.Controls.MetroLabel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.axVLCPlugin21 = new AxAXVLC.AxVLCPlugin2();
+            this.buttonPlay = new MetroFramework.Controls.MetroButton();
+            this.vlcControl1 = new Vlc.DotNet.Forms.VlcControl();
+            ((System.ComponentModel.ISupportInitialize)(this.pRAM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pCPU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vlcControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pRAM
+            // 
+            this.pRAM.CategoryName = "Memory";
+            this.pRAM.CounterName = "% Committed Bytes in Use";
+            // 
+            // pCPU
+            // 
+            this.pCPU.CategoryName = "Processor";
+            this.pCPU.CounterName = "% Processor Time";
+            this.pCPU.InstanceName = "_Total";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(554, 113);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(38, 19);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "CPU:";
+            // 
+            // metroProgressBarCPU
+            // 
+            this.metroProgressBarCPU.Location = new System.Drawing.Point(601, 109);
+            this.metroProgressBarCPU.Name = "metroProgressBarCPU";
+            this.metroProgressBarCPU.Size = new System.Drawing.Size(389, 23);
+            this.metroProgressBarCPU.TabIndex = 1;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(554, 165);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(41, 19);
+            this.metroLabel2.TabIndex = 2;
+            this.metroLabel2.Text = "RAM:";
+            // 
+            // metroProgressBarRAM
+            // 
+            this.metroProgressBarRAM.Location = new System.Drawing.Point(601, 158);
+            this.metroProgressBarRAM.Name = "metroProgressBarRAM";
+            this.metroProgressBarRAM.Size = new System.Drawing.Size(389, 26);
+            this.metroProgressBarRAM.TabIndex = 3;
+            this.metroProgressBarRAM.Click += new System.EventHandler(this.metroProgressBar2_Click);
+            // 
+            // labelCPU
+            // 
+            this.labelCPU.AutoSize = true;
+            this.labelCPU.Location = new System.Drawing.Point(1008, 113);
+            this.labelCPU.Name = "labelCPU";
+            this.labelCPU.Size = new System.Drawing.Size(31, 19);
+            this.labelCPU.TabIndex = 4;
+            this.labelCPU.Text = "0 %";
+            this.labelCPU.Click += new System.EventHandler(this.metroLabel3_Click);
+            // 
+            // labelRAM
+            // 
+            this.labelRAM.AutoSize = true;
+            this.labelRAM.Location = new System.Drawing.Point(1008, 165);
+            this.labelRAM.Name = "labelRAM";
+            this.labelRAM.Size = new System.Drawing.Size(31, 19);
+            this.labelRAM.TabIndex = 5;
+            this.labelRAM.Text = "0 %";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(554, 206);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "CPU";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "RAM";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(523, 154);
+            this.chart1.TabIndex = 6;
+            // 
+            // axVLCPlugin21
+            // 
+            this.axVLCPlugin21.Enabled = true;
+            this.axVLCPlugin21.Location = new System.Drawing.Point(12, 99);
+            this.axVLCPlugin21.Margin = new System.Windows.Forms.Padding(2);
+            this.axVLCPlugin21.Name = "axVLCPlugin21";
+            this.axVLCPlugin21.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin21.OcxState")));
+            this.axVLCPlugin21.Size = new System.Drawing.Size(521, 292);
+            this.axVLCPlugin21.TabIndex = 9;
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(149, 421);
+            this.buttonPlay.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(144, 55);
+            this.buttonPlay.TabIndex = 10;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseSelectable = true;
+            this.buttonPlay.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // vlcControl1
+            // 
+            this.vlcControl1.BackColor = System.Drawing.Color.Black;
+            this.vlcControl1.Location = new System.Drawing.Point(344, 396);
+            this.vlcControl1.Name = "vlcControl1";
+            this.vlcControl1.Size = new System.Drawing.Size(189, 103);
+            this.vlcControl1.Spu = -1;
+            this.vlcControl1.TabIndex = 11;
+            this.vlcControl1.Text = "vlcControl1";
+            this.vlcControl1.VlcLibDirectory = ((System.IO.DirectoryInfo)(resources.GetObject("vlcControl1.VlcLibDirectory")));
+            this.vlcControl1.VlcMediaplayerOptions = null;
+            this.vlcControl1.Click += new System.EventHandler(this.vlcControl1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1106, 554);
+            this.Controls.Add(this.vlcControl1);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.axVLCPlugin21);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.labelRAM);
+            this.Controls.Add(this.labelCPU);
+            this.Controls.Add(this.metroProgressBarRAM);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroProgressBarCPU);
+            this.Controls.Add(this.metroLabel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10, 60, 10, 10);
+            this.Text = " Hello ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pRAM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pCPU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vlcControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.PerformanceCounter pRAM;
+        private System.Diagnostics.PerformanceCounter pCPU;
+        private System.Windows.Forms.Timer timer;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBarCPU;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBarRAM;
+        private MetroFramework.Controls.MetroLabel labelCPU;
+        private MetroFramework.Controls.MetroLabel labelRAM;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin21;
+        private MetroFramework.Controls.MetroButton buttonPlay;
+        private Vlc.DotNet.Forms.VlcControl vlcControl1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        static string serverIp = "localhost";
+        static int port = 8080;
+        TcpClient client = new TcpClient(serverIp, port);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //float fcpu = pCPU.NextValue();
+            //float fram = pRAM.NextValue();
+            //metroProgressBarCPU.Value = (int)fcpu;
+            //metroProgressBarRAM.Value = (int)fram;
+            ////Console.WriteLine(fcpu + "," + fram);
+            //labelCPU.Text = string.Format("{0:0.00}%", fcpu);
+            //labelRAM.Text = string.Format("{0:0.00}%", fram);
+            //chart1.Series["CPU"].Points.AddY(fcpu);
+            //chart1.Series["RAM"].Points.AddY(fram);
+
+            // Send request to server
+            int byteCount = Encoding.ASCII.GetByteCount("ping") + 1;
+            byte[] sendData = new byte[byteCount];
+            // Append a ';' as the delimiter
+            sendData = Encoding.ASCII.GetBytes("ping;");
+            NetworkStream stream = client.GetStream();
+            stream.Write(sendData, 0, sendData.Length);
+
+            // Get response from server
+            StringBuilder msg = new StringBuilder();
+            byte[] receivedData = new byte[100];
+            stream.Read(receivedData, 0, receivedData.Length);
+            List<int> intList = new List<int>();
+            foreach (byte b in receivedData)
+            {
+                //Console.WriteLine(b);
+                // I'm not using null character here because I hope we have a special character that splits the fields
+                // Just like what we did in 482 project 4
+                if (b.Equals(59))
+                {
+                    intList.Add(Convert.ToInt32(msg.ToString()));
+                    Console.WriteLine(msg.ToString());
+                    msg.Clear();
+                }
+                else
+                {
+                    msg.Append(Convert.ToChar(b).ToString());
+                }
+            }
+
+            // Update the value on UI
+            int cpu = intList[0];
+            int ram = intList[1];
+            metroProgressBarCPU.Value = cpu;
+            metroProgressBarRAM.Value = ram;
+            labelCPU.Text = string.Format("{0}%", cpu);
+            labelRAM.Text = string.Format("{0}%", ram);
+            chart1.Series["CPU"].Points.AddY(cpu);
+            chart1.Series["RAM"].Points.AddY(ram);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void metroLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroProgressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Click this activeX VLC
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            axVLCPlugin21.playlist.add("https://youtu.be/MAYMFwjoWy8");
+            //  http cannot be used for live stream. Use RTSP RSP instead.
+            //axVLCPlugin21.playlist.add("https://youtu.be/dovudiWfzMU");
+            Console.WriteLine("****************************************");
+            axVLCPlugin21.playlist.play();
+        }
+
+        // Don't use this vlc.Dotnet button
+        private void vlcControl1_Click(object sender, EventArgs e)
+        {
+            vlcControl1.SetMedia("https://youtu.be/MAYMFwjoWy8");
+            vlcControl1.Play();
+        }
+    }
+}
