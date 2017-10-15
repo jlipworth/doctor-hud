5a152ff0df852d0eb28aa390358d31da0b04e202
jyqing <jyqing@umich.edu>
2017-10-15 16:28:15 -0400

    Changed fake data communication and random number generator

--- src/open-ice-script/open-ice-socket.py
+++ src/open-ice-script/open-ice-socket.py
@@ -1,24 +1,27 @@
 import socket
 import random
 from os import urandom
+import time
+import platform
 
 metric_ids = {"crap": "heart_rate"}
 
 heart_rate = 60
 systolic = 120
 diastolic = 80
-sp_o2 = 98
+sp_o2 = 96
 
-rng = random.seed(urandom(64))
+random.seed(urandom(64))
 
 
 def broadcast_fake_input():
     connect = initialize_socket()
     while True:
         generate_fake_num()
-        connect.send("1: " + heart_rate + " 2: " + systolic + " 3: "
-                     + diastolic + " 4: " + sp_o2)
-
+        metric_str = "heart_rate:" + str(heart_rate) + ";systolic:" + str(systolic) + ";diastolic:" + str(diastolic) + ";sp_o2:" + str(sp_o2) + ";"
+        print(len(metric_str))
+        connect.send(metric_str.encode(encoding='ascii'))
+        time.sleep(1)
 
 
 def broadcast_input():
@@ -38,11 +41,11 @@ def broadcast_input():
 
 
 def initialize_socket():
-    sock = socket.socket()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = socket.gethostname()
     port = 56754
     print("host: " + host + " port: " + str(port))
-    sock.bind((host, port))
+    sock.bind(("10.211.55.2", port))
     sock.listen(5)
     connect, address = sock.accept()
     return connect
@@ -50,10 +53,10 @@ def initialize_socket():
 
 def generate_fake_num():
     global heart_rate, systolic, diastolic, sp_o2
-    heart_rate += rng.randint(-2, 2)
-    systolic += rng.randint(-2, 2)
-    diastolic += rng.randint(-2, 2)
-
+    heart_rate += random.randint(-2, 2)
+    systolic += random.randint(-2, 2)
+    diastolic += random.randint(-2, 2)
+    sp_o2 += random.randint(-1,1 )
 
 if __name__ == "__main__":
     broadcast_fake_input()
