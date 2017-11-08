9335310b5d1fd36fc1992b1ca9e69e746d324251
normalsilly <normalsilly@foxmail.com>
2017-11-08 14:04:14 -0500

    display one sensor data

--- webserver/static/index.js
+++ webserver/static/index.js
@@ -1,19 +1,49 @@
+var obj;
+var metric_dict = {};
 
 (function () {
-    var metric_dict = {}
     var data_socket = new WebSocket("ws://" + location.host + "/data");
 
     data_socket.onmessage = function (e) {
         //$("#testo").html(e.data);
-        var obj = JSON.parse(e.data);
+        obj = JSON.parse(e.data);
         for (var metric in obj) {
             metric_dict[metric] = obj[metric];
         }
         document.getElementById("testo").innerHTML = "";
         for (var metric in metric_dict) {
-            document.getElementById("testo").innerHTML += metric + ": " + 
+            document.getElementById("testo").innerHTML += metric + ": " +
             metric_dict[metric] + "<br>";
         }
     };
 
 })();
+
+function gauge(dom, input, name) {
+    var option = {
+        tooltip : {
+            formatter: "{a} <br/>{b} : {c}%"
+        },
+        series: [
+            {
+                name: name,
+                type: 'gauge',
+                detail: {formatter:'{value}%'},
+                data: [{value: 50, name: 'heart rate'}]
+            }
+        ]
+    };
+    var myChart = echarts.init(dom);
+    //(Math.random() * 100).toFixed(2) - 0
+    setInterval(function () {
+            option.series[0].data[0].value = metric_dict[heart_rate];
+            myChart.setOption(option, true);
+        },2000);
+}
+
+$(document).ready(function() {
+    var input = ''
+    gauge(document.getElementById('heartRate'), input, 'heart_rate')
+});
+
+
